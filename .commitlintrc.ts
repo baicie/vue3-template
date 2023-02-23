c0096a75babf627dc7c5c3d479df48a3c1ae0c3d
刘志伟 <2055572879@qq.com>
2023-02-23 16:32:28 +0800

    feat: test

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -3,40 +3,7 @@ import type { UserConfig } from '@commitlint/types'
 const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {},
-  plugins: [
-    {
-      rules: {
-        commit: ({ header }) => {
-          const PrefixRegex = /^(\[[bBtT][0-9]+])/
-          if (!PrefixRegex.test(header)) {
-            return [
-              false,
-              `
-              \x1b[1;41;33m       您本次版本提交不符合日志提交规范!!!     \x1b[0m
-              `
-            ]
-          }
-          // max length
-          const minLen = 4
-          if (header.length < minLen) {
-            return [
-              false,
-              `Message长度不得少于${minLen}字符,当前提交message长度: ${header.length}字符`
-            ]
-          }
-
-          return [true]
-        }
-      }
-    }
-  ],
-  ignores: [(commit) => commit.startsWith('Merge ')],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(\[[bBtT][0-9]+]) (\w*)(?:\((.*)\))?!?: (.*)$/,
-      headerCorrespondence: ['type', 'scope', 'subject']
-    }
-  }
+  ignores: [(commit) => commit.startsWith('Merge ')]
 }
 
 export default config
