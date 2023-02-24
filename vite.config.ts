f14773f743c50481757e6dfeb8ad0d81658e4293
刘志伟 <2055572879@qq.com>
2023-02-24 21:34:36 +0800

    feat: postcss done

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import viteCompression from 'vite-plugin-compression';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import postcsspxtoviewport from 'postcss-px-to-viewport';
 import { visualizer } from 'rollup-plugin-visualizer';
 import AutoImport from 'unplugin-auto-import/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'node:url';
+import viteCompression from 'vite-plugin-compression';
 // 是否引入CDN
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -44,6 +45,23 @@ export default defineConfig({
     preprocessorOptions: {
       less: {
         javascriptEnabled: true
+      },
+      postcss: {
+        plugins: [
+          postcsspxtoviewport({
+            unitToConvert: 'px', // 要转化的单位
+            viewportWidth: 750, // UI设计稿的宽度
+            unitPrecision: 6, // 转换后的精度，即小数点位数
+            propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+            viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
+            fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
+            selectorBlackList: ['ignore-'], // 指定不转换为视窗单位的类名，
+            minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+            mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+            replace: true, // 是否转换后直接更换属性值
+            landscape: false // 是否处理横屏情况
+          })
+        ]
       }
     }
   },
